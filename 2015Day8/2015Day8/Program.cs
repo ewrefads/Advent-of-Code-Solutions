a2241ea9154a2bf72c815ec414cbb059eefc7edd
ewrefads <clara.donovan@outlook.com>
2025-04-27 22:58:00 +0200

    added sloution for day 8

---
+++ 2015Day8/2015Day8/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+var input = File.ReadLines("input.txt");
+
+int stringLiteralLength = 0;
+int memoryStringLength = input.Sum(l => l.Length);
+foreach(string word in input){
+    int wordLength = word.Length - 2;
+    int i = 0;
+    while(i < word.Length)
+    {
+        if(word[i] == '\\')
+        {
+            if(word[i + 1] == '\\' || word[i + 1] == '\"')
+            {
+                wordLength -= 1;
+                i += 2;
+            }
+            else if(word[i + 1] == 'x')
+            {
+                wordLength -= 3;
+                i += 4;
+            }
+        }
+        else
+        {
+            i++;
+        }
+    }
+    stringLiteralLength += wordLength;
+}
+Console.WriteLine(memoryStringLength - stringLiteralLength);
+int encodedStringLength = 0;
+foreach(string word in input){
+    int wordLength = word.Length + 2;
+    int i = 0;
+    while(i < word.Length)
+    {
+        if(word[i] == '\\' || word[i] == '\"')
+        {
+            wordLength++;
+        }
+        i++;
+    }
+    encodedStringLength += wordLength;
+}
+Console.WriteLine(encodedStringLength - memoryStringLength);
