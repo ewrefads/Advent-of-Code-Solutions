66b3e97eba9ba20f9cc06a3ff27341c00bbd8a70
ewrefads <clara.donovan@outlook.com>
2025-04-29 13:16:11 +0200

    Added solution for day 17

---
+++ 2015Day17/2015Day17/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] input = {0};
+int minCombination = int.MaxValue;
+int numMinCombinations = 0;
+int NumberOfCombinations(int index, int remainingEggnog, int usedContainers)
+{
+    if(remainingEggnog == input[index])
+    {
+        usedContainers++;
+        if(usedContainers < minCombination)
+        {
+            minCombination = usedContainers;
+            numMinCombinations = 1;
+        }
+        else if(usedContainers == minCombination)
+        {
+            numMinCombinations++;
+        }
+        return 1;
+    }
+    else if(remainingEggnog < input[index])
+    {
+        return 0;
+    }
+    else
+    {
+        int possibleCombinations = 0;
+        remainingEggnog -= input[index];
+        usedContainers++;
+        for(int i = index + 1; i < input.Length; i++)
+        {
+            possibleCombinations += NumberOfCombinations(i, remainingEggnog, usedContainers);
+        }
+        return possibleCombinations;
+    }
+}
+int totalCombinations = 0;
+for(int i = 0; i < input.Length; i++)
+{
+    totalCombinations += NumberOfCombinations(i, 150, 0);
+}
+Console.WriteLine(totalCombinations);
+Console.WriteLine(numMinCombinations);
