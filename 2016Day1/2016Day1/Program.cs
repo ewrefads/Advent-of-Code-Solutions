bb9a81fdb671a232bcc3fbfcb6ee6101fbb1db4d
ewrefads <clara.donovan@outlook.com>
2025-05-03 11:00:06 +0200

    cleanup

--- 2016Day1/2016Day1/Program.cs
+++ 2016Day1/2016Day1/Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string[] input = {"L2", "L5", "L5", "R5", "L2", "L4", "R1", "R1", "L4", "R2", "R1", "L1", "L4", "R1", "L4", "L4", "R5", "R3", "R1", "L1", "R1", "L5", "L1", "R5", "L4", "R2", "L5", "L3", "L3", "R3", "L3", "R4", "R4", "L2", "L5", "R1", "R2", "L2", "L1", "R3", "R4", "L193", "R3", "L5", "R45", "L1", "R4", "R79", "L5", "L5", "R5", "R1", "L4", "R3", "R3", "L4", "R185", "L5", "L3", "L1", "R5", "L2", "R1", "R3", "R2", "L3", "L4", "L2", "R2", "L3", "L2", "L2", "L3", "L5", "R3", "R4", "L5", "R1", "R2", "L2", "R4", "R3", "L4", "L3", "L1", "R3", "R2", "R1", "R1", "L3", "R4", "L5", "R2", "R1", "R3", "L3", "L2", "L2", "R2", "R1", "R2", "R3", "L3", "L3", "R4", "L4", "R4", "R4", "R4", "L3", "L1", "L2", "R5", "R2", "R2", "R2", "L4", "L3", "L4", "R4", "L5", "L4", "R2", "L4", "L4", "R4", "R1", "R5", "L2", "L4", "L5", "L3", "L2", "L4", "L4", "R3", "L3", "L4", "R1", "L2", "R3", "L2", "R1", "R2", "R5", "L4", "L2", "L1", "L3", "R2", "R3", "L2", "L1", "L5", "L2", "L1", "R4"};
-//string[] input = {"R8", "R4", "R4", "R8"};
+string[] input = {"insert input here"};
 int x = 0;
 int y = 0;
 int deltaX = 0;
@@ -109,4 +108,3 @@ int distance = Math.Abs(x) + Math.Abs(y);
 int twiceDistance = Math.Abs(twiceX) + Math.Abs(twiceY);
 Console.WriteLine(distance);
 Console.WriteLine(twiceDistance);
-Console.WriteLine("(" + twiceX + ", " + twiceY + ")");
